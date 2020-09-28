40fb2f60e304f477afc3e9d3464f08303a417fa1
Sheen An <sheen.an.goh@gmail.com>
2020-09-28 15:16:41 +0800

    Added all features to deck

--- cards/deck.go
+++ cards/deck.go
@@ -1,6 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"os"
+	"strings"
+	"time"
+)
 
 // Create a new type of 'deck
 // which is a slice of strings
@@ -22,9 +29,41 @@ func newDeck() deck {
 	return cards
 }
 
-// Receiver
+// Receiver Function
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
+	}
+}
+
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
+}
+
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
+}
+
+func (d deck) saveToFile(filename string) error {
+	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
+}
+
+func newDeckFromFile(filename string) deck {
+	byteslice, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+	return strings.Split(string(byteslice), ",")
+}
+
+func (d deck) shuffle() {
+
+	source := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(source)
+
+	for i := range d {
+		newPosition := r.Intn(len(d) - 1)
+		d[i], d[newPosition] = d[newPosition], d[i]
 	}
 }
--- cards/main.go
+++ cards/main.go
@@ -2,7 +2,9 @@ package main
 
 func main() {
 	cards := newDeck()
-
+	// cards.saveToFile("my_cards")
+	// cards := newDeckFromFile("my_cards")
+	cards.shuffle()
 	cards.print()
 }
 
