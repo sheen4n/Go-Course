b82aa28fa29081e9c0c5619889dfc83dd4e2ebd1
Sheen An <sheen.an.goh@gmail.com>
2020-09-28 12:50:02 +0800

    Created Cards List

--- cards/main.go
+++ cards/main.go
@@ -3,9 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	// var card string = "Ace of Spades"
-	card := newCard()
-	fmt.Println(card)
+	cards := []string{newCard(), newCard()}
+	cards = append(cards, "Six of Spades")
+
+	fmt.Println(cards)
 }
 
 func newCard() string {
