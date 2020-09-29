e8379f8996af5a0bef52c3e1aba8481fd50e2605
Sheen An <sheen.an.goh@gmail.com>
2020-09-29 16:57:20 +0800

    map vs struct

--- map/main.go
+++ map/main.go
@@ -4,14 +4,23 @@ import "fmt"
 
 func main() {
 	// var colors map[string]string
-	colors := make(map[string]string)
+	// colors := make(map[string]string)
 
-	// colors := map[string]string{
-	// 	"red":   "#ff0000",
-	// 	"green": "#4bf745",
-	// }
+	colors := map[string]string{
+		"red":   "#ff0000",
+		"green": "#4bf745",
+	}
 
 	colors["white"] = "#ffffff"
 
-	fmt.Println(colors)
+	// delete(colors, "white")
+	printMap(colors)
+
+	// fmt.Println(colors)
+}
+
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println(color, ":", hex)
+	}
 }
