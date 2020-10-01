163d1e3fd41cc7190f7e2c6595f7a424f8ebc032
sheen4n <sheen.an.goh@gmail.com>
2020-10-01 14:35:25 +0800

    go routine wait

--- channels/main.go
+++ channels/main.go
@@ -14,18 +14,27 @@ func main() {
 		"http://amazon.com",
 	}
 
+	c := make(chan string)
+
 	for _, link := range links {
-		go checkLink(link)
+		go checkLink(link, c)
 	}
+
+	for {
+		go checkLink(<-c, c)
+	}
+
 }
 
-func checkLink(link string) {
+func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link, "might be down!")
+		c <- link
 		return
 	}
 
 	fmt.Println(link, "is up!")
+	c <- link
 	return
 }
